28086ced9426547253a7b9832308dc4d9ce42afa
Ramshil <84653816+Ramshil@users.noreply.github.com>
2021-07-31 11:06:57 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -86,8 +86,7 @@ def add_task():
         conn.commit()
         return redirect(url_for("tasks.alltasks"), 302)
 
-
-    @bp.route("/<id>/edit", methods=["GET", "POST",])
+@bp.route("/<id>/edit", methods=["GET", "POST",])
 def edit_task(id): 
     conn = db.get_db()
     cursor = conn.cursor()
